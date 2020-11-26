fc589992c861b4633431c11eb3f5f54b1308645e
ViniciusRoquef <viniciusroqf@gmail.com>
2020-11-26 09:14:52 -0300

    Correcao de bug para letras duplicadas

--- main.c
+++ main.c
@@ -5,13 +5,14 @@
 // JOGO DA FORCA
 
 int main () {
-    char palavra[50], tentativa[50];
+    char palavra[50], tentativa[50], copiaPalavra[50];
     int i, fim = 0, encontrei;
     int n_tentativas = 0, limite_tentativas, corretas = 0;
     char chute, c;
 
     // busca uma palavra
     strcpy (palavra, "exemplo");
+    strcpy (copiaPalavra, palavra);
 
     // define o limite de tentativas
     limite_tentativas = strlen (palavra) * 1.5;
@@ -23,7 +24,7 @@ int main () {
     do {
         // apresenta cabeçalho do jogo
         system("cls");
-        printf("\n      FORCA EM C\n");
+        printf("\n     FORCA EM C\n");
         printf("_____________________________\n\n\n");
 
         // apresenta letras já encontradas
@@ -36,32 +37,36 @@ int main () {
             printf("___ ");
         printf("\n");
 
+        // Adiciona número de tentativas e quantas estão corretas
+        printf("\n\n\n________________________________\n\n\n");
+        printf("Restantes: %d\n", limite_tentativas - n_tentativas);
+        printf("Corretas: %d\n", corretas);
+        printf("\n\n\n________________________________\n\n\n");
+
+
         // ******** RESPOSTAS DO JOGADOR *********
         // le respostas do jogador
 
-        printf("Entre com uma letra (ou * para encerrar) + <enter>: ");
+        printf("\n\n\nEntre com uma letra (ou * para encerrar) + <enter>: ");
         scanf ("%c", &chute);
         scanf("%c", &c);
 
         // Testa-se a letra informada encontra-se na palavra escolhida
         encontrei = 0;
         for (i=0; i<strlen(palavra); i++)
-            if (palavra[i] == chute) {
-                tentativa[i] = chute;
-                corretas ++;
+            if (copiaPalavra[i] == chute) {
+                    copiaPalavra[i]= '*';
+                    tentativa[i] = chute;
+                    corretas ++;
         }
 
         if (encontrei == 0)
             n_tentativas++;
 
-        if (n_tentativas >= limite_tentativas || corretas == strlen(palavra) || chute == '*')
+        if (n_tentativas >= limite_tentativas || corretas >= strlen(palavra) || chute == '*')
             fim = 1;
 
 
     } while (fim == 0);
-
-
-
-
     return 0;
 }
